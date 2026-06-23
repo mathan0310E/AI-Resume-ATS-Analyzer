bbb1173a3164bccb08d33744459c01dbe8dbfa53
Mathan Kumar.S <115224608+mathan0310E@users.noreply.github.com>
2026-06-23 23:19:57 +0530

    Refactor routes in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,63 +7,32 @@ import ATSAnalyzer from "./pages/ATSAnalyzer";
 import History from "./pages/History";
 import ResumeUpload from "./pages/ResumeUpload";
 import Result from "./pages/Result";
-import ProtectedRoute
-from "./components/ProtectedRoute";
-import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-        <Route
-          path="/"
-          element={<Login />}
-        />
+        <Route path="/" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
 
         <Route
-          path="/register"
-          element={<Register />}
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
         />
 
-       <Route
-  path="/dashboard"
-  element={
-    <ProtectedRoute>
-      <Dashboard />
-    </ProtectedRoute>
-  }
-/>
-
-        <Route
-          path="/analyze"
-          element={<ATSAnalyzer />}
-        />
-
-        <Route
-          path="/history"
-          element={<History />}
-        />
-
-        <Route
-          path="/upload"
-          element={<ResumeUpload />}
-        />
-
-        <Route
-          path="/result"
-          element={<Result />}
-        />
+        <Route path="/analyze" element={<ATSAnalyzer />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/upload" element={<ResumeUpload />} />
+        <Route path="/result" element={<Result />} />
 
       </Routes>
-      <Route
-  path="/profile"
-  element={
-    <ProtectedRoute>
-      <Profile />
-    </ProtectedRoute>
-  }
-/>
     </BrowserRouter>
   );
 }
